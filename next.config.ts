1f4113f963b9182cb07b828fd824f1c8ad924732
beast hunterr <hunterrbeast@gmail.com>
2025-05-18 08:16:49 +0000

    https://img.freepik.com/premium-vector/contact-us-sticker-button-label-badge-flag-sign-symbol_1027249-262.jpg?semt=ais_hybrid&w=740 add this image in contact us

--- next.config.ts
+++ next.config.ts
@@ -166,6 +166,12 @@ const nextConfig: NextConfig = {
         hostname: 'media.istockphoto.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
